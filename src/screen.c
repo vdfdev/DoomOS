c1c562fdc7ac28c69c1783d3e2e3ce83e46bfb44
vdf <me@vdf.dev>
2023-01-28 15:51:42 -0800

    Fixes screen flickering

--- src/screen.c
+++ src/screen.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdbool.h>
 #include "kernel.h"
 #include "terminal.h"
 
@@ -18,35 +19,31 @@ void clear_screen() {
   }
 }
 
-void write_cute_str(uint8_t x, uint8_t y, char* s) {
-  uint8_t j = 0;
-  for (uint32_t i = 0; *(s+i) != 0; i++) {
-    char c = *(s+i);
-    write_char(x + i, y, c, 2 + (j%4));
-    j++;
-    if (c == ' ') {
-      j = 0;
+void write_line(uint8_t x, uint8_t y, char* s) {
+  bool line_ended = false;
+  for (uint32_t i = 0; i < SCREEN_COLS; i++) {
+    if (line_ended) {
+      write_char(x + i, y, ' ', 7);
+      continue;
     }
-  }
-}
-
-void write_str(uint8_t x, uint8_t y, char* s) {
-  for (uint32_t i = 0; *(s+i) != 0; i++) {
-    write_char(x + i, y, *(s+i), 7);
+    char c = *(s+i);
+    if (c == 0) {
+      line_ended = true;
+      c = ' ';
+    }
+    write_char(x + i, y, c, 7);
   }
 }
 
 void screen_init() {
   clear_screen();
-  write_cute_str(0, 0, "DoomOS loading...");
+  write_line(0, 0, "DoomOS loading...");
   kprint("[SCREEN] OK\r\n");
 }
 
 void screen_refresh() {
-  clear_screen();
   for (uint16_t i = 0; i < max(terminal_line_count, SCREEN_ROWS); i++) {
     char *line = (char*)terminal_get_line(i);
-    write_str(0, SCREEN_ROWS - 1 - i, line);
+    write_line(0, SCREEN_ROWS - 1 - i, line);
   }
-  write_cute_str(SCREEN_COLS-6, 0, "DoomOS");
 }
